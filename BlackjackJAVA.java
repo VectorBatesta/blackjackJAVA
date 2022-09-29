ffc412fa6bcf11e221ccd39fb6d06313dabdd6ec
VectorBatesta <48493157+VectorBatesta@users.noreply.github.com>
2022-09-29 17:34:01 -0300

    init
    didn't have time to do anything so I created the classes

---
+++ BlackjackJAVA.java
@@ -0,0 +1,26 @@
+package blackjackjava;
+
+
+
+class Deck {
+    
+}
+class Card {
+    
+}
+class Hand {
+    
+}
+class Player {
+    
+}
+class Bank {
+    
+}
+
+
+public class BlackjackJAVA {
+    public static void main(String[] args) {
+        
+    }
+}
